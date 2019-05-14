a7ea5b5eb19b1ffc295e3eb927c682e8e0e29cb8
WayneSan <waynesan@zerozone.tw>
2019-05-14 19:46:05 +0800

    Minor release 1.0.1

--- replace.js
+++ replace.js
@@ -1,15 +1,19 @@
+const REGEXP_REGEXP = /\/((?![*+?])(?:[^\r\n\[\\]|\\.|\[(?:[^\r\n\]\\]|\\.)*\])+)\/((?:g(?:im?|mi?)?|i(?:gm?|mg?)?|m(?:gi?|ig?)?)?)/;
+const REGEXP_SIMPLE = /((?![*+?])(?:[^\r\n\[\\]|\\.|\[(?:[^\r\n\]\\]|\\.)*\])+)/;
+
 module.exports.templateTags = [{
     name: 'replace',
     displayName: 'Replace String',
     description: 'Replace a string by a replacing pattern or Regular Expression',
     args: [
         {
-          displayName: 'Pattern Type',
-          type: 'enum',
-          options: [
-            {displayName: 'Normal', value: 'substr'},
-            {displayName: 'Regular Expression', value: 'regexp'}
-          ]
+            displayName: 'Pattern Type',
+            type: 'enum',
+            options: [
+                {displayName: 'Normal', value: 'substr'},
+                {displayName: 'Regular Expression', value: 'regexp'},
+                {displayName: 'Simple Regular Expression (without flags)', value: 'simpleregexp'}
+            ]
         },
         {
             displayName: 'Replacing Pattern',
@@ -21,24 +25,44 @@ module.exports.templateTags = [{
             type: 'string'
         },
         {
+            displayName: 'Replace Type',
+            type: 'enum',
+            options: [
+                {displayName: 'Normal', value: ''},
+                {displayName: 'Case Insensitive', value: 'i'},
+                {displayName: 'Recursive', value: 'g'},
+                {displayName: 'Case Insensitive & Recursive', value: 'ig'},
+            ],
+            hide: args => args[0].value !== 'simpleregexp'
+        },
+        {
             displayName: 'Source String',
             type: 'string'
         }
     ],
-    async run (context, type, pattern, as, source) {
+    async run (context, patternType = 'substr', pattern, asString, replaceType, source) {
         source = source || '';
 
         if (pattern.length == 0) {
-            throw new Error('Replacing Pattern field is required');
+            throw new Error('Replacing Pattern field is required.');
         }
-        if (type === 'regexp') {
-            var isRegExp = /^\/(.+)\/([gimsuy]?)$/.exec(pattern);
-            if (isRegExp != null && isRegExp.length == 3) {
+
+        if (patternType === 'simpleregexp') {
+            if (REGEXP_SIMPLE.exec(pattern) === null) {
+                throw new Error('Invalid Simple Regular Expression syntax.');
+            }
+            pattern = new RegExp(pattern, replaceType);
+        } else if (patternType === 'regexp') {
+            var isRegExp = REGEXP_REGEXP.exec(pattern);
+            if (isRegExp === null) {
+                throw new Error('Invalid Regular Expression syntax.');
+            } else if (isRegExp.length == 3) {
                 pattern = new RegExp(isRegExp[1], isRegExp[2]);
             } else {
-                pattern = new RegExp(isRegExp == null ? pattern : isRegExp[1]);
+                pattern = new RegExp(isRegExp[1]);
             }
         }
-        return source.replace(pattern, as);
+
+        return source.replace(pattern, asString);
     }
 }];
